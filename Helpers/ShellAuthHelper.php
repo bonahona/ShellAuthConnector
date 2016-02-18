afedd982c433ffe9d1221d876fbdc4851bb2b0c3
SoftronicBona <bjorn.fyrvall@softronic.se>
2016-02-18 18:38:42 +0100

    Added new calls to the backend. Added a new local user model to map application data to.

--- Helpers/ShellAuthHelper.php
+++ Helpers/ShellAuthHelper.php
@@ -7,8 +7,9 @@ class ShellAuthHelper implements  IHelper
     public $ShellAuthServer;
     public $ShellAuthPort;
     public $ShellAuthMethodPaths;
+    public $Controller;
 
-    public function Init($config)
+    public function Init($config, $controller)
     {
         $this->ApplicationName = $config['ShellApplication']['Name'];
         $this->PublicKey = $config['ShellApplication']['PublicKey'];
@@ -16,18 +17,32 @@ class ShellAuthHelper implements  IHelper
         $this->ShellAuthServer = $config['ShellAuthServer']['Server'];
         $this->ShellAuthPort = $config['ShellAuthServer']['Port'];
         $this->ShellAuthMethodPaths = $config['ShellAuthServer']['MethodPaths'];
+
+        $this->Controller = $controller;
     }
 
-    public function CreateApplication($applicationName)
+    public function CreateApplication($application)
     {
         $payLoad = array(
-            'ShellApplication' => array(
-                'ApplicationName' => $applicationName
-            )
+            'ShellApplication' => $application
         );
 
         $callPath = $this->GetApplicationPath('CreateApplication');
-        $this->SendToServer($payLoad, $callPath);
+        return $this->SendToServer($payLoad, $callPath);
+    }
+
+    public function GetApplication($id = null)
+    {
+        if($id == null){
+            $payLoad = array();
+        }else{
+            $payLoad = array(
+                'Id' => $id
+            );
+        }
+
+        $callPath = $this->GetApplicationPath('GetApplication');
+        return $this->SendToServer($payLoad, $callPath);
     }
 
     public function CreateUser($username, $displayName, $password)
@@ -41,27 +56,91 @@ class ShellAuthHelper implements  IHelper
         );
 
         $callPath = $this->GetApplicationPath('CreateUser');
-        $this->SendToServer($payLoad, $callPath);
+        return $this->SendToServer($payLoad, $callPath);
     }
 
     public function Login($username, $password)
     {
+        $payLoad = array(
+            'ShellUser' => array(
+                'Username' => $username,
+                'Password' => $password,
+            )
+        );
+
         $callPath = $this->GetApplicationPath('Login');
-        var_dump($callPath);
-        return "Works";
+        $response =  $this->SendToServer($payLoad, $callPath);
+
+        if($response['Error'] == 0){
+            $this->Controller->Session['SessionToken'] = $response['Data']['AccessToken'];
+            $this->Controller->SetLoggedInUser($response['Data']['User']);
+        }
+
+        return $response;
     }
 
-    public function Logout()
+    public function Logout($accessToken = null)
     {
+        if($accessToken == null){
+            $accessToken = $this->Controller->Session['SessionToken'];
+        }
+        $payLoad = array(
+            'AccessToken' => $accessToken
+        );
+
         $callPath = $this->GetApplicationPath('Logout');
-        var_dump($callPath);
-
-        return "";
+        $this->Controller->Session->Destroy();
+        return $this->SendToServer($payLoad, $callPath);
     }
 
-    public function DummyFunction()
+    public function CheckAccessToken($accessToken = null)
     {
-        return "Dummy function";
+        if($accessToken == null){
+            $accessToken = $this->Controller->Session['SessionToken'];
+        }
+        $payLoad = array(
+            'AccessToken' => $accessToken
+        );
+
+        $callPath = $this->GetApplicationPath('CheckAccessToken');
+        return $this->SendToServer($payLoad, $callPath);
+    }
+
+    public function GetUser($id = null)
+    {
+        if($id != null){
+            $payLoad = array(
+                'Id' => $id
+            );
+        }else{
+            $payLoad = array();
+        }
+
+        $callPath = $this->GetApplicationPath('GetUser');
+        return $this->SendToServer($payLoad, $callPath);
+    }
+
+    public function GetLocalUsers()
+    {
+        $callPath = $this->GetApplicationPath('GetLocalUsers');
+        return $this->SendToServer(array(), $callPath);
+    }
+
+    public function SetPrivilegeLevel($userLevel, $userId, $applicationId = null)
+    {
+        $payLoad = array(
+            'ShellUserPrivilege' => array(
+                'ShellUserId' => $userId,
+                'UserLevel' => $userLevel
+            )
+        );
+
+        if($applicationId != null){
+            $payLoad['ShellUserPrivilege']['ShellApplicationId'] = $applicationId;
+        }
+
+        $callPath = $this->GetApplicationPath('SetPrivilegeLevel');
+        return $this->SendToServer($payLoad, $callPath);
     }
 
     protected function GetApplicationPath($callName)
@@ -74,7 +153,7 @@ class ShellAuthHelper implements  IHelper
         return $result;
     }
 
-    protected  function SendToServer($payload, $callPath)
+    protected function SendToServer($payload, $callPath)
     {
         $data = array(
             'ShellAuth' => array(
@@ -91,11 +170,24 @@ class ShellAuthHelper implements  IHelper
         curl_setopt($curl, CURLOPT_URL, $callPath);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_USERAGENT, "ShellAuthConnector");
         curl_setopt($curl, CURLOPT_POSTFIELDS, array(
             'data' => $data
         ));
 
-        $response = curl_exec($curl);
-        var_dump($response);
+        if(!$response = curl_exec($curl)){
+            $curlError = curl_error($curl);
+            //$curlErrorCode = curl_errno($curl);
+
+            return array(
+                'Error' => 1,
+                    'ErrorList' => array(
+                        $curlError
+                    )
+            );
+        }
+
+        curl_close($curl);
+        return json_decode($response, true);
     }
 }
